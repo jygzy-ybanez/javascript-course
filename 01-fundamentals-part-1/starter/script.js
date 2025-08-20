ef0392cd8422aff195b58ccc50f857779942cb03
jygzy-yabnez <jvybanez.work@gmail.com>
2025-08-21 02:43:50 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,64 @@
-// JavaScript Fundamentals - Part 1
-// We'll write our code here!
+// // JavaScript Fundamentals - Part 1
+// // We'll write our code here!
+
+// let js ="amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("==== VARIABLES ===");
+
+// let firstName = "Jygzy";
+// console.log(firstName);
+
+// let age = 22;
+// age = 23;
+// console.log(age);
+
+// const birthYear = 2002;
+// console.log(birthYear);
+
+// const PI = 3.1415;
+// console.log(PI);
+
+// const country = "Philippines";
+// const language = "Filipino";
+// const population = 10;
+
+// let myCurrentJob = "Student";
+
+// console.log("=== DATA TYPES");
+
+// let age = 23;
+// console.log(age);
+// console.log(typeof age);
+
+// let firstName = "Jygzy";
+// console.log("firstName");
+// console.log(typeof firstName);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// let dynamicVariable = 23;
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+// dynamicVariable = "Now I'm a string!";
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+// dynamicVariable = true;
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+// EXERCISE 1: PERSONAL INFO VARIABLES
+// 1. Your first name (const)
+const firstName = "Jygzy";
+console.log("My Name is: ", firstName);
+
+// 2. Your age (let - it changes every year!)
+// 3. Your favorite number (const)
+// 4. Whether you like JavaScript so far (boolean)
+
+// Log each variable and its type to the console
