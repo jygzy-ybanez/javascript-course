8fc1b310e080e41ba76d16696b85c4e2e5dba56b
jygzy-yabnez <jvybanez.work@gmail.com>
2025-08-21 11:03:21 +0800

    feat: implement operators and expressions
    - Add mathematical operators (+, -, *, /, **)
    - Implement assignment operators (+=, -=, *=, /=, ++, --)
    - Create comparison operators for boolean logic
    - Understand operator precedence and parentheses
    - Complete BMI Calculator coding challenge with real-world application

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,64 +1,101 @@
-// // JavaScript Fundamentals - Part 1
-// // We'll write our code here!
+// // // JavaScript Fundamentals - Part 1
+// // // We'll write our code here!
 
-// let js ="amazing";
-// console.log(40 + 8 + 23 - 10);
+// // let js ="amazing";
+// // console.log(40 + 8 + 23 - 10);
 
-// console.log("==== VARIABLES ===");
+// // console.log("==== VARIABLES ===");
 
-// let firstName = "Jygzy";
+// // let firstName = "Jygzy";
+// // console.log(firstName);
+
+// // let age = 22;
+// // age = 23;
+// // console.log(age);
+
+// // const birthYear = 2002;
+// // console.log(birthYear);
+
+// // const PI = 3.1415;
+// // console.log(PI);
+
+// // const country = "Philippines";
+// // const language = "Filipino";
+// // const population = 10;
+
+// // let myCurrentJob = "Student";
+
+// // console.log("=== DATA TYPES");
+
+// // let age = 23;
+// // console.log(age);
+// // console.log(typeof age);
+
+// // let firstName = "Jygzy";
+// // console.log("firstName");
+// // console.log(typeof firstName);
+
+// // let javascriptIsFun = true;
+// // console.log(javascriptIsFun);
+// // console.log(typeof javascriptIsFun);
+
+// // let year;
+// // console.log(year);
+// // console.log(typeof year);
+
+// // let dynamicVariable = 23;
+// // console.log(dynamicVariable, typeof dynamicVariable);
+
+// // dynamicVariable = "Now I'm a string!";
+// // console.log(dynamicVariable, typeof dynamicVariable);
+
+// // dynamicVariable = true;
+// // console.log(dynamicVariable, typeof dynamicVariable);
+
+// // === EXERCISE 1: PERSONAL INFO VARIABLES ===
+// // 1. Your first name (const)
+// const firstName = "Jygzy";
 // console.log(firstName);
+// console.log(typeof firstName);
 
-// let age = 22;
-// age = 23;
-// console.log(age);
-
-// const birthYear = 2002;
-// console.log(birthYear);
-
-// const PI = 3.1415;
-// console.log(PI);
-
-// const country = "Philippines";
-// const language = "Filipino";
-// const population = 10;
-
-// let myCurrentJob = "Student";
-
-// console.log("=== DATA TYPES");
-
+// // 2. Your age (let - it changes every year!)
 // let age = 23;
 // console.log(age);
 // console.log(typeof age);
 
-// let firstName = "Jygzy";
-// console.log("firstName");
-// console.log(typeof firstName);
+// // 3. Your favorite number (const)
+// const favNumber = 14;
+// console.log(favNumber);
+// console.log(typeof favNumber);
 
-// let javascriptIsFun = true;
-// console.log(javascriptIsFun);
-// console.log(typeof javascriptIsFun);
+// // 4. Whether you like JavaScript so far (boolean)
+// const likesJavaScript = true;
+// console.log(likesJavaScript);
+// console.log(typeof likesJavaScript);
 
-// let year;
-// console.log(year);
-// console.log(typeof year);
+// // === EXERCISE 2: VARIABLE REASSIGNMENT ===
+// // 1. Create a let variable called 'score' with value 100
+// let score = 100;
+// // 2. Log it to the console
+// console.log(score);
+// // 3. Change it to 150
+// score = 200;
+// // 4. Log it again
+// console.log(score);
+// // 5. Try the same with const - what happens?
+// // Assignment to constant varilable error
 
-// let dynamicVariable = 23;
-// console.log(dynamicVariable, typeof dynamicVariable);
+// // === EXERCISE 3: DATA TYPE DETECTIVE ===
+// // Use typeof to check the data type of these values:
+// console.log(typeof 42);
+// console.log(typeof "Hello");
+// console.log(typeof true);
+// console.log(typeof undefined);
 
-// dynamicVariable = "Now I'm a string!";
-// console.log(dynamicVariable, typeof dynamicVariable);
+// // What do you think each one will return?
+// // 42 = Number - 42 is numerical
+// // Hello = String - Contains only alphabetical symbols
+// // true = boolean - Choices can contain only true or false
+// // undefined = undefined - No Value
 
-// dynamicVariable = true;
-// console.log(dynamicVariable, typeof dynamicVariable);
 
-// EXERCISE 1: PERSONAL INFO VARIABLES
-// 1. Your first name (const)
-const firstName = "Jygzy";
-console.log("My Name is: ", firstName);
-
-// 2. Your age (let - it changes every year!)
-// 3. Your favorite number (const)
-// 4. Whether you like JavaScript so far (boolean)
-
-// Log each variable and its type to the console
